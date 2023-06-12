beb22ba76c66b2d08c81fdce2641d224d86cbea7
mike <mjowiti@gmail.com>
2023-06-12 23:39:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
-function scuberGreetingForFeet(feet){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-  let ride
-  if(feet<=400){
-    ride = `This one is on me!`;
-    return ride;
+  if (ride <= 400) {
+    return `This one is on me!`;
+  } 
+  else if (ride > 2000 && ride <=2500) {
+    return `I will gladly take your thirty bucks.`;
+  } else{
+    return `No can do.`
   }
-  else if(feet>2000){
-    ride =`I will gladly take your thirty bucks.`;
-    return ride;
-  }
-  else if(feet>2500){
-    ride = `No can do.`;
-    return ride;
-  }
-  }
+
+} 
+  
 scuberGreetingForFeet(199)
 scuberGreetingForFeet(2001)
 scuberGreetingForFeet(2501)
 
 function ternaryCheckCity(city){
-  // Write your code here!
-  let answer
-  if(city=`NYC`){
-  answer =`Ok, sounds good.`;
-  return answer;
-}
-else if(city!=`NYC`){
-  answer = `No go.`;
-  return answer;
-}
+  let trueStatement=`Ok, sounds good.`
+  let falseStatement=`No go.`
+  return (city === 'NYC') ? trueStatement : falseStatement
+  
 }
 ternaryCheckCity(`NYC`)
 ternaryCheckCity(`Pittsburgh`)
 
 function switchOnCharmFromTip(tip){
   // Write your code here!
-  let appreciation
-  if(tip =`generous`){
-    appreciation = `Thank you so much.`;
-    return appreciation;
-    }
-    else if(tip=`not as generous`){
-      appreciation = `Thank you.`;
-      return appreciation
-    }
-    else{
-      appreciation =`Bye.`;
-      return appreciation;
-    }
-    
-}
+  
+  
+
+  switch (tip){
+    case `generous`:
+      return `Thank you so much.`;
+      break;
+      case 'not as generous':
+        return `Thank you.`
+        break;
+        default:
+          return`Bye.`
+  }
+   
+  }
+
 switchOnCharmFromTip(`generous`)
 switchOnCharmFromTip(`not as generous`)
 switchOnCharmFromTip(`thanks for everything`)
